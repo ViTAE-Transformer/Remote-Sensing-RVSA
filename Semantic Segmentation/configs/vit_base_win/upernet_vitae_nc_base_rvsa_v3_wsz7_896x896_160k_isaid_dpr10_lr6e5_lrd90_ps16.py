3899ef20b96799e553f7cb8e8a2f4fd2bacaa9c9
DotWang <35402675+DotWang@users.noreply.github.com>
2022-10-10 23:05:30 +0800

    init

---
+++ Semantic Segmentation/configs/vit_base_win/upernet_vitae_nc_base_rvsa_v3_wsz7_896x896_160k_isaid_dpr10_lr6e5_lrd90_ps16.py
@@ -0,0 +1,122 @@
+_base_ = [
+    '../_base_/models/upernet_vit_base_win.py', #'../_base_/datasets/potsdam.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+
+dataset_type = 'isAIDDataset'
+data_root = '/workspace/CV/users/wangdi153/Dataset/isaid_patches'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (896, 896)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    dict(type='Resize', img_scale=(896, 896), ratio_range=None, keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size),
+    dict(type='RandomFlip',prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(896, 896),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=True,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='Pad', size=(896,896), pad_val=0, seg_pad_val=255),
+            dict(type='RandomFlip',prob=1.0),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=None,
+        img_ratios=[1.0],
+        flip=True,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train/images',
+        ann_dir='train/labels',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='val/images',
+        ann_dir='val/labels',
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='val/images',
+        ann_dir='val/labels',
+        pipeline=val_pipeline))
+        
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optimizer = dict(_delete_=True, type='AdamW', lr=6e-5, betas=(0.9, 0.999), weight_decay=0.05,
+                 constructor='LayerDecayOptimizerConstructor', 
+                 paramwise_cfg=dict(
+                        num_layers=12, 
+                        layer_decay_rate=0.9,
+                            )
+                            )
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+ 
+optimizer_config = dict(grad_clip=None)
+
+model = dict(
+    pretrained='../mae-main/output/mae_vitae_base_pretrn/millionAID_224/1600_0.75_0.00015_0.05_2048/checkpoint-1599-transform-no-average.pth',
+    backbone=dict(
+        type='ViTAE_NC_Win_RVSA_V3_WSZ7',
+        img_size=896,
+        patch_size=16,
+        drop_path_rate=0.1,
+        out_indices=[3, 5, 7, 11],
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        use_abs_pos_emb=True
+    ),
+    decode_head=dict(
+        num_classes=16,
+        ignore_index=255
+    ),
+    auxiliary_head=dict(
+        num_classes=16,
+        ignore_index=255
+    ),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole')
+    )
---
+++ Semantic Segmentation/mmseg/models/backbones/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .bisenetv1 import BiSeNetV1
+from .bisenetv2 import BiSeNetV2
+from .cgnet import CGNet
+from .erfnet import ERFNet
+from .fast_scnn import FastSCNN
+from .hrnet import HRNet
+from .icnet import ICNet
+from .mit import MixVisionTransformer
+from .mobilenet_v2 import MobileNetV2
+from .mobilenet_v3 import MobileNetV3
+from .resnest import ResNeSt
+from .resnet import ResNet, ResNetV1c, ResNetV1d
+from .resnext import ResNeXt
+from .stdc import STDCContextPathNet, STDCNet
+from .swin import SwinTransformer
+from .timm_backbone import TIMMBackbone
+from .twins import PCPVT, SVT
+from .unet import UNet
+from .vit import VisionTransformer
+from .our_resnet import Our_ResNet
+from .ViTAE_Window_NoShift.base_model import ViTAE_Window_NoShift_basic
+from .swin_transformer import swin
+from .ViTAE_DeformWindow_NoShift_v1.ViTAE_DeformWindow_NoShift_v1 import ViTAE_DeformWindow_NoShift_v1_basic
+
+
+
+from .vit_win_rvsa_v3_wsz7 import ViT_Win_RVSA_V3_WSZ7
+from .vit_win_rvsa_v3_kvdiff_wsz7 import ViT_Win_RVSA_V3_KVDIFF_WSZ7
+
+from .vitae_nc_win_rvsa_v3_wsz7 import ViTAE_NC_Win_RVSA_V3_WSZ7
+from .vitae_nc_win_rvsa_v3_kvdiff_wsz7 import ViTAE_NC_Win_RVSA_V3_KVDIFF_WSZ7
+
+__all__ = [
+    'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
+    'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3',
+    'VisionTransformer', 'SwinTransformer', 'MixVisionTransformer',
+    'BiSeNetV1', 'BiSeNetV2', 'ICNet', 'TIMMBackbone', 'ERFNet', 'PCPVT',
+    'SVT', 'STDCNet', 'STDCContextPathNet','ViTAE_Window_NoShift_basic',
+    'Our_ResNet', 'swin','ViTAE_DeformWindow_NoShift_v1_basic', 
+    'ViT_Win_RVSA_V3_WSZ7', 'ViT_Win_RVSA_V3_KVDIFF_WSZ7',
+    'ViTAE_NC_Win_RVSA_V3_WSZ7', 'ViTAE_NC_Win_RVSA_V3_KVDIFF_WSZ7'
+]
